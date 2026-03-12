0bcda7d36e7fcb57bcdc4bcb1c4c5ce3acfae4af
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-12 16:41:33 +0100

    Fix

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.RateLimiting;
 using Scalar.AspNetCore;
 using System.Threading.RateLimiting;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
